0287deee5f5db9f6e85aa69d96ca0323182f3bc7
MariskaPutri18 <mariskaputri960@gmail.com>
2025-09-30 21:41:44 +0700

    Praktikum 4

--- lib/basic_widget/image_widget.dart
+++ lib/basic_widget/image_widget.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 
 class MyImageWidget extends StatelessWidget {
-  const MyImageWidget({Key? key}) : super(key: key);
+  const MyImageWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Image(
-      image: AssetImage("logo_polinema.jpg")
-    );
+    return const Image(image: AssetImage("images/logo_polinema.png"));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world/basic_widget/text_widget.dart';
+import 'basic_widget/image_widget.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -104,7 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            MyTextWidget(),
+            const MyImageWidget(),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
